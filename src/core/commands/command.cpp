3ec6beacc5ceb4af1d1669c638d5b593a642cf45
qqwl2 <qqwl2@mail.ru>
2026-04-14 06:20:10 +0600

    minor changes

--- src/core/commands/command.cpp
+++
@@ -1,8 +0,0 @@
-#include <include/entities/station.hpp>
-#include <include/graphics/Iinterface.hpp>
-
-class Command {
-public:
-  virtual ~Command() = default;
-  virtual void execute(Station& station, Iinterface& interface) = 0;
-};
